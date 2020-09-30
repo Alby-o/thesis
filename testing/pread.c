eda8f5595ed6090ff6c313ce98a5410005d091aa
Alexander Blyth <alexanderblyth5@gmail.com>
2020-09-30 12:30:33 +1000

    Update from while(true) to while(1)

--- testing/pread.c
+++ testing/pread.c
@@ -12,7 +12,7 @@ int main() {
     int r1 = 0;
     int r2 = 0;          //            {L(r2)=False} 
 
-  while(true) {
+  while(1) {
    do {
                                //             {_invariant: r1 % 2 == 0 /\ r1 <= z}
                                //             {_Gamma: r1 -> LOW, r2 -> (r1 == z), z -> LOW}
