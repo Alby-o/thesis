32c48fd8b5498a70d5c466363b28d71ad73230c2
Alexander Blyth <alexanderblyth5@gmail.com>
2021-05-09 07:43:50 +1000

    WIP

--- annotater/wpif.py
+++ annotater/wpif.py
@@ -1,9 +1,26 @@
+import re
+
 class Wpif:
     def __init__(self, line, line_number):
         self.line = line.strip()
         self.line_number = line_number
     
     def transform(self):
+        # Find all vars
+        vars = re.findall('var\(.*?\)', self.line)
+        varNames = {}
+        
+        # Extract variable names
+        for i, var in enumerate(vars):
+            if var in varNames:
+                continue
+
+            varName = var[var.find("var(") + 4 : var.find(")")]
+            varNames[var] = varName
+        
+        # Replace var names in line
+        
+        print(varNames)
         return "TRANSFORMED LINE"
 
     def __str__(self):
