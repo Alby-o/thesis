dfd8d5249bf0424960e53c939e394734ed55d03d
Alexander Blyth <alexanderblyth5@gmail.com>
2021-05-11 15:27:22 +1000

    Extract program running to separate function

--- annotater/annotater.py
+++ annotater/annotater.py
@@ -10,29 +10,32 @@ parser.add_argument('--output', default='out.s', help='Annotated output')
 parser.add_argument('--optimisation', default=0, choices=[0, 1, 2, 3], type=int, help='optimisiation level')
 
 def main(args):
+    run(args.filename, args.output, args.optimisation)
+
+def run(filename, output, optimisation):
     create_temp_folder()
-    source = Source(args.filename)
+    source = Source(filename)
 
     # Clone our source to begin with so we do not modify the original file
     source = source.clone()
     # Compile as-is to see output with optimisation
-    assembly = source.compile(args.optimisation)
+    assembly = source.compile(optimisation)
     
     # Transpile & insert inline assembly
     source.transpile()
-    assemblyAnnotated = source.compile(args.optimisation, output=args.output)
+    assemblyAnnotated = source.compile(optimisation, output=output)
     if (assembly.data != assemblyAnnotated.data):
         print(os.linesep + "Warning: There was a difference in the compiled outputs when annotated." + os.linesep)
     else:
-        print(args.output)
+        print(output)
     for line in difflib.unified_diff(assembly.data, assemblyAnnotated.data, fromfile='assembly', tofile='annotated', lineterm=''):
         print(line)
+    cleanup_temp_folder()
 
 if __name__ == "__main__":
     args = parser.parse_args()
     try:
         main(args)
-        cleanup_temp_folder()
     except KeyboardInterrupt:
         print('Interrupted')
         cleanup_temp_folder()
