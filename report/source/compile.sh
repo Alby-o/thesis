ebd623127abb1276227ddf347124d7a2c378f4f3
Alexander Blyth <alexanderblyth5@gmail.com>
2021-06-03 19:51:28 +1000

    Add arh to output to subdirectory

--- report/source/compile.sh
+++ report/source/compile.sh
@@ -2,8 +2,6 @@
 for file in *.c
 do
 filename="${file%.*}"
-$1 *.c -S -O0
-mv $filename.s $filename-O0.s
-$1 *.c -S -O3
-mv $filename.s $filename-O3.s
+$1 $file -S -O0 -o $2$filename-O0.s
+$1 $file -S -O3 -o $2$filename-O3.s
 done
