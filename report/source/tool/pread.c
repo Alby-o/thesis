1ecd780fbc281e22bdd2b7c305e308b85d1539fd
Alexander Blyth <alexanderblyth5@gmail.com>
2021-06-06 13:41:12 +1000

    Execution WIP

---
+++ report/source/tool/pread.c
@@ -0,0 +1,30 @@
+volatile int z;
+volatile int x;
+
+int main() {
+    // security policies:
+    // annotation: L(var(z)) = true
+    // annotation: L(var(x)) = var(z) % 2 == 0
+    // annotation: var(x) < var(x)'
+    int r1 = 0;
+    int r2 = 0;
+    // annotation: _P_0: var(r1) % 2 == 0
+    // annotation: _Gamma_0: var(r1) -> LOW, var(r2) -> LOW
+    // annotation: L(var(r2)) = false
+
+    while(1) {
+        do {
+            // annotation: _invariant: var(r1) % 2 == 0 /\ var(r1) <= z
+            // annotation: _Gamma: var(r1) -> LOW, var(r2) -> (var(r1) == var(z)), var(z) -> LOW
+            do {
+                // annotation: _invariant: var(r1) <= var(z)
+                // annotation: _Gamma: var(r1) -> LOW
+                r1 = z;
+            } while (r1 %2 != 0);
+            r2 = x;
+        } while (z != r1);
+    }
+    return r2;
+}
+
+
---
+++ report/source/tool/test.c
@@ -0,0 +1,6 @@
+int main()
+{
+    int i = 0;
+    // annotation: L(var(i)) = High
+    return i + 1;
+}
